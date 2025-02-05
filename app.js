48310aee0b24976fe31690f330134a3db27a1624
Gabriel <gabrieldemaj22@gmail.com>
2025-02-05 13:54:37 +0100

    acceptin data from req

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit();
+  //   console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      'body <form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk", chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.writeHead(302, { Location: "/" });
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
